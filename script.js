d2ffb908e0579c9299ca6b573dddb513e237d72d
Carlo <26264+carlotrimarchi@users.noreply.github.com>
2021-07-06 13:28:18 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,17 @@
+let a = 5;
+let b = 10;
+
+console.log("a", a); // 5
+console.log("b", b); // 10
+
+/* Write your code here so that the values of "a" and "b" are swapped and console.log them again. */
+
+
+
+let mainColor = "tomato";
+let secondaryColor = "limegreen";
+
+console.log("mainColor", mainColor);
+console.log("secondaryColor", secondaryColor);
+
+/* Write your code here to invert the colors, so that mainColor will be limegreen and secondaryColor tomato. */
